c35ba7b85db73d53838f02943d2ba840d9c7318c
Mr_mohit <mohitgiri852004@gmail.com>
2024-10-05 21:28:05 +0530

    url change

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-68n1.onrender.com",
     credentials: true,
   })
 );
--- frontend/src/utlis/constants.js
+++ frontend/src/utlis/constants.js
@@ -1,4 +1,8 @@
-export const  USER_API_END_POINT = "http://localhost:8000/api/v1/user"
-export const  JOB_API_END_POINT = "http://localhost:8000/api/v1/job"
-export const  COMPANIES_API_END_POINT = "http://localhost:8000/api/v1/company"
-export const  APPLICATIONS_API_END_POINT = "http://localhost:8000/api/v1/application"
+export const USER_API_END_POINT =
+  "https://job-portal-68n1.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://job-portal-68n1.onrender.com/api/v1/job";
+export const COMPANIES_API_END_POINT =
+  "https://job-portal-68n1.onrender.com/api/v1/company";
+export const APPLICATIONS_API_END_POINT =
+  "https://job-portal-68n1.onrender.com/api/v1/application";
