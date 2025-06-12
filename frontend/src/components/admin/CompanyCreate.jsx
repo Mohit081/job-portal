761d05661600ae55df8b51fe722a93ce322a7815
Mr_mohit <136919454+Mohit081@users.noreply.github.com>
2025-06-12 18:00:05 +0530

    Update CompanyCreate.jsx

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -28,7 +28,7 @@ const CompanyCreate = () => {
       );
       if (res?.data?.success) {
          dispatch(setSingleCompany(res.data.company));
-        toast.success(res.data.message);
+        toast.success(res.data.data.message);
         const companyId = res?.data?.data?._id;
         navigate(`/admin/companies/${companyId}`);
       }
