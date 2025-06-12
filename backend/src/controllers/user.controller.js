39e2cb4509c3319971d53c36e8145c52f9767b5d
Mr_mohit <136919454+Mohit081@users.noreply.github.com>
2025-06-12 17:05:58 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -34,13 +34,10 @@ const userRegister = asyncHandler(async (req, res) => {
   }
   console.log(fullName,email,phoneNumber,role,password)
 
-  try{
 const file = req.file;
   const fileUri = getDataUri(file);
   const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
   console.log("cloudinary mai dikkar hai ",cloudResponse.secure_url)
-  }catch(error){
-console.log("cloudinary rr",error)}
   
   const existedUser = await User.findOne({ email });
 
